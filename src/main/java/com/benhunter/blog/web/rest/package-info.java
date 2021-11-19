b3ad7309599930ee0f317ab336482f788023a8a8
Ben Hunter <code@benhunter.me>
2021-11-19 05:55:01 -0600

    Initial version of JHipsterDemoBlog generated by generator-jhipster@7.3.1

---
+++ src/main/java/com/benhunter/blog/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.benhunter.blog.web.rest;
---
+++ src/main/java/com/benhunter/blog/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.benhunter.blog.web.rest.vm;
